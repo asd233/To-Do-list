af39b8df7711a037d873f555211b317a73c31276
asd233 <2053044516@qq.com>
2020-07-05 10:38:09 +0800

    为span添加事件，以便重新设置事件

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 ; (function () {
+    let that;
     function Item(value) {
         this.value = value;
+        this.li = document.createElement("li");
+        that = this;
     };
     Item.prototype.render = function () {
-        let li = document.createElement("li");
         let unfinished = document.querySelector("#unfinished");
         let completed = document.querySelector("#completed");
         let unfinishedNum = document.querySelector("#unfinishedNum");
@@ -18,49 +20,64 @@
             if (this.checked) {
                 span.style.textDecoration = "line-through";
                 span.style.color = "#778899";
-                li.style.webkitFilter = "grayscale(100%)";
-                completed.append(li);
+                that.li.style.webkitFilter = "grayscale(100%)";
+                completed.append(that.li);
             } else {
                 span.style.textDecoration = "none";
                 span.style.color = "#000";
-                li.style.webkitFilter = "grayscale(0%)";
-                unfinished.append(li);
+                that.li.style.webkitFilter = "grayscale(0%)";
+                unfinished.append(that.li);
             }
             unfinishedNum.innerText = unfinished.childNodes.length;
             completedNum.innerText = completed.childNodes.length;
         };
-        li.appendChild(input);
+        this.li.appendChild(input);
 
         //填入事项
         var span = document.createElement("span");
-        span.innerText = this.value;
-        li.appendChild(span);
+        span.innerHTML = this.value;
+        span.onclick = function () {
+            let input = document.createElement("input");
+            input.type = "text";
+            input.value = that.value;
+            span.innerText = "";
+            input.onblur = function () {
+                if (input.value) {
+                    that.value = input.value;
+                    span.innerText = that.value;
+                    input.remove();
+                } else {
+                    span.innerText = that.value;
+                    input.remove();
+                }
+            }
+            that.li.insertBefore(input, span);
+            input.focus();
+        }
+        this.li.appendChild(span);
 
         //创建删除标签
         var a = document.createElement("a");
         a.href = "#";
         a.innerText = "-";
         a.onclick = function () {
-            li.remove();
+            that.li.remove();
             unfinishedNum.innerText = unfinished.childNodes.length;
             completedNum.innerText = completed.childNodes.length;
         }
 
 
-        li.appendChild(a);
-        unfinished.appendChild(li);
+        this.li.appendChild(a);
+        unfinished.appendChild(this.li);
         unfinishedNum.innerText = unfinished.childNodes.length;
     };
 
-    title.onkeydown = function (event) {
-        //回车键添加事件
-        if (event.keyCode == 13) {
-            if (title.value) {
-                let title = document.querySelector("#title")
-                let one = new Item(title.value);
-                one.render(title);
-                title.value = "";
-            }
+    title.onblur = function () {
+        if (title.value) {
+            let title = document.querySelector("#title")
+            let one = new Item(title.value);
+            one.render(title);
+            title.value = "";
         }
 
         window.Item = Item;
