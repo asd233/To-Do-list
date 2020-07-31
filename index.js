280fb1e228a1b1bdb49ca254eca4d231752ebe6a
asd233 <2053044516@qq.com>
2020-07-31 12:58:43 +0800

    将部分原生代码转为jQuery语法

--- index.js
+++ index.js
@@ -1,85 +1,80 @@
 ; (function () {
-    let that;
-    function Item(value) {
-        this.value = value;
-        that = this;
-    };
-    Item.prototype.render = function () {
-        let li = document.createElement("li");
-        let unfinished = document.querySelector("#unfinished");
-        let completed = document.querySelector("#completed");
-        let unfinishedNum = document.querySelector("#unfinishedNum");
-        var completedNum = document.querySelector("#completedNum");
-        //创建复选框
-        let input = document.createElement("input");
-        input.type = "checkbox";
-        input.className = "check";
-
-        //复选框绑定事件
-        input.onclick = function () {
-            if (this.checked) {
-                span.style.textDecoration = "line-through";
-                span.style.color = "#778899";
-                li.style.webkitFilter = "grayscale(100%)";
-                completed.append(li);
-            } else {
-                span.style.textDecoration = "none";
-                span.style.color = "#000";
-                li.style.webkitFilter = "grayscale(0%)";
-                unfinished.append(li);
-            }
-            unfinishedNum.innerText = unfinished.childNodes.length;
-            completedNum.innerText = completed.childNodes.length;
+    $(function () {
+        let that;
+        function Item(value) {
+            this.value = value;
+            that = this;
         };
-        li.appendChild(input);
+        Item.prototype.render = function () {
+            let li = document.createElement("li");
+            let unfinished = document.querySelector("#unfinished");
+            let completed = document.querySelector("#completed");
+            let unfinishedNum = document.querySelector("#unfinishedNum");
+            var completedNum = document.querySelector("#completedNum");
 
-        //填入事项
-        var span = document.createElement("span");
-        span.innerHTML = this.value;
-        span.onclick = function () {
-            let input = document.createElement("input");
-            input.type = "text";
-            input.value = that.value;
-            span.innerText = "";
-            input.onblur = function () {
-                if (input.value) {
-                    that.value = input.value;
-                    span.innerText = that.value;
-                    input.remove();
+            //创建复选框
+            let input = $("<input>");
+            input.prop({ "type": "checkbox", "className": "check" })
+
+            //复选框绑定事件
+            input.on("click", function () {
+                if (this.checked) {
+                    li.style.webkitFilter = "grayscale(100%)";
+                    span.css({ "textDecoration": "line-through", "color": "#778899" })
+                    completed.append(li);
                 } else {
-                    span.innerText = that.value;
-                    input.remove();
+                    span.css({ "textDecoration": "none", "color": "#000" });
+                    li.style.webkitFilter = "grayscale(0%)";
+                    unfinished.append(li);
                 }
-            }
-            li.insertBefore(input, span);
-            input.focus();
-        }
-        li.appendChild(span);
+                unfinishedNum.innerText = unfinished.childNodes.length;
+                completedNum.innerText = completed.childNodes.length;
+            });
+            li.append(input[0]);
 
-        //创建删除标签
-        var a = document.createElement("a");
-        a.href = "#";
-        a.innerText = "-";
-        a.onclick = function () {
-            li.remove();
+            //填入事项
+            let span = $("<span></span>");
+            span.text(this.value);
+            span.on("click", function () {
+                let input = $("<input>");
+                input.prop({ "type": "text", "value": that.value })
+                span.text("");
+                input.on("blur", function () {
+                    if (input.val()) {
+                        that.value = input.val();
+                        span.text(that.value);
+                        input.remove();
+                    } else {
+                        span.text(that.value);
+                        input.remove();
+                    }
+                })
+                span.before(input);
+                input.focus();
+            })
+            li.append(span[0]);
+
+            //创建删除标签
+            var a = $("<a />");
+            a.prop("href", "#").text("-");
+            a.on("click", function () {
+                li.remove();
+                unfinishedNum.innerText = unfinished.childNodes.length;
+                completedNum.innerText = completed.childNodes.length;
+            })
+
+            li.append(a[0]);
+            unfinished.appendChild(li);
             unfinishedNum.innerText = unfinished.childNodes.length;
-            completedNum.innerText = completed.childNodes.length;
+        };
+        title.onblur = function () {
+            if (title.value) {
+                let one = new Item(title.value);
+                one.render(title);
+                title.value = "";
+            }
+
+            window.Item = Item;
         }
-
-
-        li.appendChild(a);
-        unfinished.appendChild(li);
-        unfinishedNum.innerText = unfinished.childNodes.length;
-    };
-
-    title.onblur = function () {
-        if (title.value) {
-            let title = document.querySelector("#title")
-            let one = new Item(title.value);
-            one.render(title);
-            title.value = "";
-        }
-
-        window.Item = Item;
-    }
+    })
 })()
